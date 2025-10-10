f45b57bca1305baf5c63081755ff16b813e6aed2
Dexter <dexter@Dexters-MacBook-Air.local>
2025-10-10 09:28:46 +0530

    fixed the code and made to use Node **map instead

--- hashmap.cpp
+++ hashmap.cpp
@@ -1,8 +1,10 @@
 /*
-A simple implementation of a hashmap in C++.
-This implementation uses separate chaining for collision resolution.
+A simple implementation of a hashmap in C++, using a bucket of pointers to
+nodes. This implementation uses separate chaining for collision resolution and
+traversal.
 */
 
+#include <climits>
 #include <iostream>
 
 // a class to represent a node in the linked list key/value pair
@@ -26,18 +28,19 @@ Node::Node() {
   this->next = nullptr;
 }
 
-Node::~Node() {}
 Node::Node(std::string key, int value) {
   this->key = key;
   this->value = value;
   this->next = nullptr;
 }
 
+Node::~Node() { std::cout << "node has been destroyed\n"; }
 //
 
 class hashmap {
 private:
-  Node *map; //! need to converted into **map instead to work.
+  //? a pointer-to-pointer array to create the 2D array for buckets.
+  Node **map;
   int size;
   int capacity;
   int hash(std::string key);
@@ -54,7 +57,7 @@ public:
   void deletePair(std::string key);
   int getValue(std::string key);
   int getSize();
-  int loadFactor();
+  double loadFactor();
 
   // TODO implement a print2D()
   //  void print2D();
@@ -63,13 +66,29 @@ public:
 hashmap::hashmap() {
   this->capacity = 10;
   this->size = 0;
-  map = new Node[capacity];
+
+  //? allocate first block of memory
+  map = new Node *[capacity];
+
+  for (size_t i = 0; i < capacity; i++) {
+
+    // assigned to null to prevent stray and dangling pointers.
+    map[i] = nullptr;
+  }
 }
 
 hashmap::hashmap(int size) {
   this->capacity = size;
   this->size = 0;
-  map = new Node[capacity];
+
+  //
+  map = new Node *[capacity];
+
+  for (size_t i = 0; i < capacity; i++) {
+
+    // assigned to null to prevent stray and dangling pointers.
+    map[i] = nullptr;
+  }
 }
 
 //? calculating the hash value by the sum of ascii values.
@@ -91,16 +110,18 @@ void hashmap::insertPair(Node *node) {
   int address = hash(node->key);
 
   // wrong key combination
-  if (address < 0 || address > capacity)
-    return;
+  if (address < 0 || address >= capacity) {
 
-  node->next = map[address].next;
-  map[address].next = node;
+    std::cout << "index cannot be calculated\n";
+    return;
+  }
+
+  node->next = map[address];
+  map[address] = node;
+  ++this->size;
 
   std::cout << "(" << node->key << ", " << node->value
             << ") has been inserted \n";
-
-  ++this->size;
 }
 
 void hashmap::lookUp(std::string key) {
@@ -108,14 +129,16 @@ void hashmap::lookUp(std::string key) {
   int address = hash(key);
 
   // wrong key combination
-  if (address < 0 || address > capacity)
-    return;
+  if (address < 0 || address >= capacity) {
 
-  Node *temp = map[address].next;
+    std::cout << "index cannot be calculated\n";
+    return;
+  }
+
+  Node *temp = map[address];
 
   while (temp) {
 
-    std::cout << "(" << temp->key << ", " << temp->value << ")\n";
     if (temp->key == key) {
       std::cout << "(" << temp->key << ", " << temp->value << ")\n";
       return;
@@ -131,10 +154,13 @@ void hashmap::updatePair(std::string key, int newValue) {
   int address = hash(key);
 
   // wrong key combination
-  if (address < 0 || address > capacity)
-    return;
+  if (address < 0 || address >= capacity) {
 
-  Node *temp = map[address].next;
+    std::cout << "index cannot be calculated\n";
+    return;
+  }
+
+  Node *temp = map[address];
 
   while (temp) {
 
@@ -145,6 +171,8 @@ void hashmap::updatePair(std::string key, int newValue) {
     }
     temp = temp->next;
   }
+
+  std::cout << "node not found\n";
 }
 
 int hashmap::getValue(std::string key) {
@@ -152,10 +180,13 @@ int hashmap::getValue(std::string key) {
   int address = hash(key);
 
   // wrong key combination
-  if (address < 0 || address > capacity)
-    return INT16_MIN;
+  if (address < 0 || address >= capacity) {
 
-  Node *temp = map[address].next;
+    std::cout << "index cannot be calculated\n";
+    return INT16_MIN;
+  }
+
+  Node *temp = map[address];
 
   while (temp) {
 
@@ -174,19 +205,36 @@ void hashmap::deletePair(std::string key) {
   int address = hash(key);
 
   // wrong key combination
-  if (address < 0 || address > capacity)
-    return;
+  if (address < 0 || address >= capacity) {
 
-  Node *temp = map[address].next;
+    std::cout << "index cannot be calculated\n";
+    return;
+  }
+
+  Node *temp = map[address];
   Node *prev = nullptr;
+
+  // head node to be deleted
+  if (temp->key == key) {
+
+    map[address] = temp->next;
+    delete temp;
+    --this->size;
+
+    std::cout << "head re-allocated\n";
+    return;
+  }
 
   while (temp) {
 
     if (temp->key == key) {
 
-      prev = temp->next;
+      prev->next = temp->next;
       std::cout << "pair has been detached \n";
+      delete temp;
       --this->size;
+
+      //
       return;
     }
     prev = temp;
@@ -198,24 +246,58 @@ void hashmap::deletePair(std::string key) {
 }
 
 int hashmap::getSize() { return this->size; }
-int hashmap ::loadFactor() { return this->size / this->capacity; }
+
+//
+double hashmap ::loadFactor() {
+  return (double)this->size / (double)this->capacity;
+}
 
 //! deallocate the memory of the bucket/map.
-hashmap::~hashmap() { delete[] this->map; }
+hashmap::~hashmap() {
+
+  // deallacote the memory of the hashmap bucket by bucket
+
+  for (size_t i = 0; i < capacity; i++) {
+
+    Node *current = map[i];
+    while (current) {
+
+      //? move the current node index to temp, jump to next node then delete
+      // temp;
+      Node *temp = current;
+      current = current->next;
+      --this->size;
+      delete temp;
+    }
+  }
+
+  //! should always done last.
+  delete[] map;
+  std::cout << " hashmap de-allocated from memory\n";
+}
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
 
-  Node *node1 = new Node("12", 5);
-  Node *node2 = new Node("15", 23);
+  //? for every "new" there must be a "delete"
+  Node *node2 = new Node("mat", 23);
+  Node *node1 = new Node("alex", 5);
+  Node *node3 = new Node("ron", 0);
+  Node *node4 = new Node("axel", 31);
+
+  //
   hashmap hashset;
   hashset.insertPair(node1);
 
   std::cout << "size of hashmap is: " << hashset.getSize() << "\n";
   hashset.insertPair(node2);
   std::cout << "size of hashmap is: " << hashset.getSize() << "\n";
+  hashset.updatePair("mat", 100);
+  hashset.insertPair(node4);
 
-  hashset.lookUp("12");
-  //
+  hashset.lookUp("ron");
+  hashset.lookUp("axel");
+
+  // delete node1, node2, node3, node4; //! keep this commented.
   return 0;
 }
